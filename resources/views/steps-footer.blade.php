8e5f01a0ff541c7c01671b2c54fab95029cef0d9
Alberto <info@albertoperipolli.com>
2022-03-31 12:07:45 +0200

    Footer Ui refactor and button labels translations

--- resources/views/steps-footer.blade.php
+++ resources/views/steps-footer.blade.php
@@ -1,10 +1,10 @@
-<div>
-    @if($this->hasPrevStep())
-        <x-button dark label="Back" wire:click="goToPrevStep" spinner="goToPrevStep"/>
-    @endif
+<div class="p-2 flex flex-row-reverse justify-between">
     @if($this->hasNextStep())
-        <x-button primary wire:click="goToNextStep" spinner="goToNextStep" label="Next"/>
+        <x-button lg primary  right-icon="chevron-right" wire:click="goToNextStep" spinner="goToNextStep" :label="__('Next')"/>
     @else
-        <x-button primary type="submit" spinner="submit" label="Submit"/>
+        <x-button lg primary type="submit" spinner="submit" :label="__('Submit')"/>
+    @endif
+    @if($this->hasPrevStep())
+        <x-button lg dark :label="__('Back')" icon="chevron-left" wire:click="goToPrevStep" spinner="goToPrevStep"/>
     @endif
 </div>
